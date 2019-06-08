5fe07ebdff821b98f519ed3dbb40b6af8d36a884
Kevin O'Brien <kob123@hotmail.co.uk>
2019-06-08 08:34:10 +0100

    Website7 - file functions

---
+++ website7/index.php
@@ -0,0 +1,89 @@
+<?php
+
+$path = '/dir1/myfile.php';
+$file = 'file1.txt';
+
+/*
+// Return filename
+echo basename($path);
+
+// Return filename without extension
+echo basename($path, '.php');
+
+// return the dirname from path
+echo dirname($path);
+
+// Check if file or folder exists (returns 1 if exists, nothing if not)
+echo file_exists($file);
+
+// Only checks if a file exists (returns 1 if exists, nothing if not)
+echo is_file($file);
+
+// Gets absolute path
+echo realpath($file);
+
+// Returns nothing if file is read-only
+echo is_writable($file);
+
+// Returns nothing if file is doesn't exist or can't be read
+echo is_readable($file);
+
+// Get filesize in bytes
+echo filesize($file);
+
+// Create a directory
+mkdir('testing');
+
+// Remove directory (if empty)
+rmdir('testing');
+
+// Copy file (creating new file if doesn't already exist)
+copy('file1.txt', 'file2.txt');
+
+// Rename file
+rename('file2.txt', 'myfile.txt');
+
+// Delete file
+unlink('myfile.txt');
+
+// Write entire file to string
+echo file_get_contents($file);
+
+// Writes string to file. Replaces existing file contents. Returns file size in bytes.
+echo file_put_contents($file, 'Goodbye World');
+
+// Write string to file without replacing exisiting content
+$current = file_get_contents($file);
+$current .= ' Goodbye World';
+file_put_contents($file, $current);
+
+// Opens entire file or URL for reading
+$handle = fopen($file, 'r');
+$data = fread($handle, filesize($file));
+echo $data;
+fclose($handle);
+
+// Open file for writing (but overwrites exisiting contents)
+$handle = fopen($file, 'w');
+$txt = 'Text written to file...';
+fwrite($handle, $txt);
+fclose($handle);
+
+// Opens file for writing (creates it if it doesn't exist)
+$handle = fopen('file2.txt', 'w');
+$txt = "Text written to file...\n";
+fwrite($handle, $txt);
+$txt = "More text written to file...\n";
+fwrite($handle, $txt);
+fclose($handle);
+
+*/
+
+
+
+
+
+
+
+
+
